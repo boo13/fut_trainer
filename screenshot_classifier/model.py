c59861a81d5ed6164154f0c5ec2f2054b5cbe3b5
randy <randynewslettercatcher@gmail.com>
2019-12-13 00:21:43 -0500

    .

--- screenshot_classifier/model.py
+++ screenshot_classifier/model.py
@@ -7,25 +7,66 @@ import numpy as np
 
 np.set_printoptions(precision=4)
 
-train_dataset = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
-test_dataset = tf.data.Dataset.from_tensor_slices((test_examples, test_labels))
+# For the file dir walking code, thanks to https://github.com/jrosebr1/imutils
+import os
 
-BATCH_SIZE = 64
-SHUFFLE_BUFFER_SIZE = 100
+image_types = (".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff")
 
-train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
-test_dataset = test_dataset.batch(BATCH_SIZE)
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
+def list_images(basePath, contains=None):
+    # return the set of files that are valid
+    return list_files(basePath, validExts=image_types, contains=contains)
 
-model.compile(optimizer=tf.keras.optimizers.RMSprop(),
-                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
-model.fit(train_dataset, epochs=10)
+def list_files(basePath, validExts=None, contains=None):
+    # loop over the directory structure
+    for (rootDir, dirNames, filenames) in os.walk(basePath):
+        # loop over the filenames in the current directory
+        for filename in filenames:
+            # if the contains string is not none and the filename does not contain
+            # the supplied string, then ignore the file
+            if contains is not None and filename.find(contains) == -1:
+                continue
 
-model.evaluate(test_dataset)
+            # determine the file extension of the current file
+            ext = filename[filename.rfind(".") :].lower()
+
+            # check to see if the file is an image and should be processed
+            if validExts is None or ext.endswith(validExts):
+                # construct the path to the image and yield it
+                imagePath = os.path.join(rootDir, filename)
+                yield imagePath
+
+
+
+
+
+train_data = list(list_images("../data/classifier"))
+
+
+print(train_data)
+
+print(f"[INFO] {len(train_data)} training images found")
+
+# train_dataset = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
+# test_dataset = tf.data.Dataset.from_tensor_slices((test_examples, test_labels))
+
+# BATCH_SIZE = 64
+# SHUFFLE_BUFFER_SIZE = 100
+#
+# train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+# test_dataset = test_dataset.batch(BATCH_SIZE)
+#
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Flatten(input_shape=(28, 28)),
+#     tf.keras.layers.Dense(128, activation='relu'),
+#     tf.keras.layers.Dense(10, activation='softmax')
+# ])
+#
+# model.compile(optimizer=tf.keras.optimizers.RMSprop(),
+#                 loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+#                 metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+#
+# model.fit(train_dataset, epochs=10)
+#
+# model.evaluate(test_dataset)
